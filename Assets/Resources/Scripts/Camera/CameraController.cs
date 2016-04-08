a891646936276ec8637213bf3116af9b0dc45685
Kiril Kafadarov <k.kafadarov@jacobs-university.de>
2016-04-09 01:42:35 +0200

    initial camera scripts

---
+++ Assets/Resources/Scripts/Camera/CameraController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+
+	public Transform target;
+	public float smoothing;
+	public bool cameraOff = false;
+
+	Vector3 offset;
+	// Use this for initialization
+	void Start ()
+	{
+		offset = transform.position - target.position;
+	}
+
+	public void setCameraOn(bool up)
+	{
+		if (up == cameraOff) {
+			return;
+		}
+
+		if (up == true) {
+			// switch other cameras off
+			// switch this one on
+		} else {
+			// switch this one off
+			// switch others on
+		}
+	}
+
+	void FixedUpdate ()
+	{
+		if (cameraOff == false) {
+			Vector3 targetCamPos = target.position + offset;
+			this.gameObject.transform.position = Vector3.Lerp (transform.position, targetCamPos, smoothing * Time.deltaTime);
+		}
+	}
+}
