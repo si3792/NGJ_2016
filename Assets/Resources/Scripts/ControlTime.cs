d0a68acdf2fe4eada30f41092d150fd0af6119a1
Simo Iliev <si3792@h37.eduroam-2a.wlan.aalborguniversitet.dk>
2016-04-09 00:29:30 +0200

    Added time controller

---
+++ Assets/Resources/Scripts/ControlTime.cs
@@ -0,0 +1,38 @@
+﻿﻿using UnityEngine;
+using System.Collections;
+
+public class ControlTime : MonoBehaviour {
+
+	public bool paused = false;
+	public float scalar = 1;
+	public bool changeTime = true;
+
+
+
+	// Update is called once per frame
+	void Update () {
+
+		// Change time scale
+		if(changeTime) {
+			changeTime = false;
+
+			Time.timeScale = scalar;
+			Time.fixedDeltaTime = 0.02F * Time.timeScale;
+		}
+
+		// Pause/Unpause game
+		if(Input.GetKeyDown(KeyCode.P)) {
+			if(paused) {
+				Time.timeScale = 1;
+				Time.fixedDeltaTime = 0.02F;
+				paused = false;
+			} else {
+				Time.timeScale = 0;
+				Time.fixedDeltaTime = 0;
+				paused = true;
+			}
+
+		}
+
+	}
+}
