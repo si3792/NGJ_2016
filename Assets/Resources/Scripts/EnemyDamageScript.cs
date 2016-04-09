5c5ad1d7800294721926dd35997d4698bb1c824f
Simo Iliev <si3792@h37.eduroam-2a.wlan.aalborguniversitet.dk>
2016-04-09 11:28:34 +0200

    Enemies drop nuggets on death

--- Assets/Resources/Scripts/EnemyDamageScript.cs
+++ Assets/Resources/Scripts/EnemyDamageScript.cs
@@ -5,6 +5,7 @@ public class EnemyDamageScript : MonoBehaviour {
 
 
 	float health = 50;
+	public GameObject biomassDrop;
 
 	void Start () {
 	
@@ -14,6 +15,7 @@ public class EnemyDamageScript : MonoBehaviour {
 	void LateFixedUpdate () {
 
 		if (health <= 0)
+			Instantiate (biomassDrop, transform.position, Quaternion.Euler (Vector3.zero));
 			Destroy (gameObject.transform.parent.gameObject);
 	}
 
