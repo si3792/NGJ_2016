f7b9ca5d34f8d0b272e6c5dfef0559b6ed199da1
Kiril Kafadarov <k.kafadarov@jacobs-university.de>
2016-04-09 05:15:41 +0200

    playing with scaling camera

--- Assets/Resources/Scripts/Camera/CrossControl.cs
+++ Assets/Resources/Scripts/Camera/CrossControl.cs
@@ -11,9 +11,6 @@ public enum modes
 public class CrossControl : MonoBehaviour
 {
 
-	public float scalingThreshold;
-	public float splitThreshold;
-
 	public modes curMode = modes.FollowPl1;
 	Transform pl1;
 	Transform pl2;
@@ -39,15 +36,10 @@ public class CrossControl : MonoBehaviour
 			transform.position = pl2.position;
 		}
 		else if (curMode == modes.FollowBoth) {
-			float distance = Vector3.Distance (pl1.position, pl2.position);
-
-			if (distance < scalingThreshold) {
-				//middle of two players
-				transform.position = new Vector3 (pl1.position.x + (pl2.position.x - pl1.position.x)/2, 
-					transform.position.y, transform.position.z
-				);
-			}
-
+			//middle of two players
+			transform.position = new Vector3 (pl1.position.x + (pl2.position.x - pl1.position.x)/2, 
+				transform.position.y, transform.position.z
+			);
 		}
 
 	}
