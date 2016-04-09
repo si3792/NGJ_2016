be61864a1dffa46bd977f8a674822fc2fea4424c
Rosen Zhelev <roro.omg@gmail.com>
2016-04-09 11:52:13 +0100

    swarm AI fix

--- Assets/Resources/Scripts/EnemyRegister.cs
+++ Assets/Resources/Scripts/EnemyRegister.cs
@@ -3,21 +3,46 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class EnemyRegister : MonoBehaviour {
+	public class EnemyReference
+	{
+		public GameObject enemy;
+		public bool valid;
+		public EnemyReference(GameObject e)
+		{
+			enemy = e;
+			valid = true;
+		}
+	}
 
+	public EnemyReference r; 
+	EnemyMovement mov;
 	// Use this for initialization
 	void Start () {
-	
+		r = new EnemyReference (gameObject);
+		mov = GetComponentInParent<EnemyMovement> ();
 	}
-	void OnTriggerStay2D(Collider2D coll) 
+	void OnTriggerEnter2D(Collider2D coll) 
 	{
 		EnemyRegister other_enemy = coll.gameObject.GetComponent<EnemyRegister> ();
-		if( other_enemy )
+		if( other_enemy && mov )
 		{
-			GetComponentInParent<EnemyMovement>().addEnemyInfluance(coll.gameObject.transform.position);
+			mov.enemies.Add(other_enemy.r);
+		}
+	}
+	void OnTriggerExit2D(Collider2D coll) 
+	{
+		EnemyRegister other_enemy = coll.gameObject.GetComponent<EnemyRegister> ();
+		if( other_enemy && mov )
+		{
+			mov.enemies.Remove(other_enemy.r);
 		}
 	}
 	// Update is called once per frame
 	void Update () {
 	
+	}
+	void OnDestroy()
+	{
+		r.valid = false;
 	}
 }
