fcbf0d488f2857e9e51faaa9f429a6449d4ea392
Simo Iliev <si3792@h37.eduroam-2a.wlan.aalborguniversitet.dk>
2016-04-09 13:45:48 +0200

    Rainbow bugs - genetic variance

---
+++ Assets/Resources/Scripts/GeneticVariance.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GeneticVariance : MonoBehaviour {
+
+	public GameObject animObj;
+
+	void Start () {
+
+		//randomize color
+		Color c = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+		foreach(SpriteRenderer sr in animObj.GetComponentsInChildren<SpriteRenderer>()) {
+			sr.material.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+		}
+
+		//randomize size
+		var scalar = Random.Range(0.7f, 1.3f);
+		transform.localScale = new Vector3(transform.localScale.x * scalar, transform.localScale.y, 0 );
+	
+	}
+
+	void Update () {
+	
+	}
+}
