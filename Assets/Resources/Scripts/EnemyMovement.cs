90456a369234d1c12d3327ff15a130d8dc17d51d
Rosen Zhelev <roro.omg@gmail.com>
2016-04-09 11:00:38 +0100

    Swarm AI fix/regress

--- Assets/Resources/Scripts/EnemyMovement.cs
+++ Assets/Resources/Scripts/EnemyMovement.cs
@@ -7,18 +7,36 @@ public class EnemyMovement : MonoBehaviour {
 	GameObject pl1,pl2;
 	float col_radius;
 	Rigidbody2D myRB;
-	EnemyRegister enemy_reg;
 	// Use this for initialization
 	void Start () {
 		
 		pl1 = GameObject.FindGameObjectWithTag ("Pl1");
 		pl2 = GameObject.FindGameObjectWithTag ("Pl2");
-		enemy_reg = GetComponentInChildren<EnemyRegister> ();
 		col_radius = GetComponent<CircleCollider2D> ().radius;
 		myRB = GetComponent<Rigidbody2D> ();
+
+		totalPush = Vector2.zero;
+		contenders = 0;
 	}
+	int contenders;
+	Vector2 totalPush;
+	public void addEnemyInfluance(Vector3 pos)
+	{
+		Vector2 push = transform.position - pos;
 
+		//calculate how much we are pushed away from this obstacle, the closer, the more push
+		float distance = Vector2.Distance(transform.position, pos) - 2*col_radius;
+		//only use push force if this object is close enough such that an effect is needed
+		++contenders; //note that this object is actively pushing
 
+		if (distance < 0.0001f) //prevent division by zero errors and extreme pushes
+		{
+			distance = 0.0001f;
+		}
+		float weight = 1 / distance;
+
+		totalPush += push * weight;
+	}
 	void FixedUpdate()
 	{
 		float targetDist = Vector2.Distance(transform.position, pl1.transform.position);
@@ -32,28 +50,6 @@ public class EnemyMovement : MonoBehaviour {
 		if (targetDist > 1)
 		{
 			Vector2 pull = (target.transform.position - transform.position) * (1 /  targetDist); //the target tries to 'pull us in'
-			Vector2 totalPush = Vector2.zero;
-
-			int contenders = 0;
-			foreach (GameObject enemy in enemy_reg.enemies)
-			{
-
-				//draw a vector from the obstacle to the ship, that 'pushes the ship away'
-				Vector2 push = transform.position - enemy.transform.position;
-
-				//calculate how much we are pushed away from this obstacle, the closer, the more push
-				float distance = Vector2.Distance(transform.position, enemy.transform.position) - 2*col_radius;
-				//only use push force if this object is close enough such that an effect is needed
-				++contenders; //note that this object is actively pushing
-
-				if (distance < 0.0001f) //prevent division by zero errors and extreme pushes
-				{
-					distance = 0.0001f;
-				}
-				float weight = 1 / distance;
-
-				totalPush += push * weight;
-			}
 
 			pull *= Mathf.Max(1, 4 * contenders); //4 * contenders gives the pull enough force to pull stuff trough (tweak this setting for your game!)
 			pull += totalPush;
@@ -63,6 +59,9 @@ public class EnemyMovement : MonoBehaviour {
 			//Set the ships new position;
 			myRB.AddForce( pull * speed * Time.deltaTime, ForceMode2D.Impulse );
 		}
+		totalPush = Vector2.zero;
+		contenders = 0;
+
 	}
 	// Update is called once per frame
 	void Update () {
--- Assets/Resources/Scripts/EnemyRegister.cs
+++ Assets/Resources/Scripts/EnemyRegister.cs
@@ -4,38 +4,20 @@ using System.Collections.Generic;
 
 public class EnemyRegister : MonoBehaviour {
 
-	Collider2D swarm_col;
-	public List<GameObject> enemies;
 	// Use this for initialization
 	void Start () {
-		enemies = new List<GameObject> ();
-		swarm_col = GetComponent<CircleCollider2D> ();
+	
 	}
-	void OnTriggerEnter2D(Collider2D coll) 
+	void OnTriggerStay2D(Collider2D coll) 
 	{
 		EnemyRegister other_enemy = coll.gameObject.GetComponent<EnemyRegister> ();
 		if( other_enemy )
 		{
-			enemies.Add (coll.gameObject);
-		}
-	}
-	void OnTriggerExit2D(Collider2D coll)
-	{
-		EnemyRegister other_enemy = coll.gameObject.GetComponent<EnemyRegister> ();
-		if( other_enemy )
-		{
-			enemies.Remove(coll.gameObject);
+			GetComponentInParent<EnemyMovement>().addEnemyInfluance(coll.gameObject.transform.position);
 		}
 	}
 	// Update is called once per frame
 	void Update () {
 	
-	}
-
-	void OnDestroy()
-	{
-		foreach (GameObject enemy in enemies) {
-			enemy.GetComponent<EnemyRegister>().enemies.Remove(gameObject);
-		}
 	}
 }
