a8f262b6c6d716bc7bca84dc70cb09677b511afa
Simo Iliev <si3792@h37.eduroam-2a.wlan.aalborguniversitet.dk>
2016-04-09 10:27:11 +0200

    Bullets now KILL

--- Assets/Resources/Scripts/BulletMovement.cs
+++ Assets/Resources/Scripts/BulletMovement.cs
@@ -1,13 +1,15 @@
-﻿﻿using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 
 public class BulletMovement : MonoBehaviour {
 
 	public float Speed;
+	public float damage = 50;
 
 	void Update()
 	{
 		this.gameObject.transform.position += transform.up * Time.deltaTime * Speed;
 
 	}
+
 }
---
+++ Assets/Resources/Scripts/EnemyDamageScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyDamageScript : MonoBehaviour {
+
+
+	float health = 50;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (health <= 0)
+			Destroy (gameObject.transform.parent.gameObject);
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if(other.gameObject.tag  == "Bullet") {
+			health -= other.gameObject.GetComponent<BulletMovement> ().damage;
+			Destroy (other.gameObject);
+		}
+	}
+
+
+}
