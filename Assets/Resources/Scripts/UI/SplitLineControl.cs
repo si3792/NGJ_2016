8d79732b217f362a9200256239cfcd2023ee6c98
Kiril Kafadarov <k.kafadarov@jacobs-university.de>
2016-04-10 00:07:45 +0200

    added middle line

---
+++ Assets/Resources/Scripts/UI/SplitLineControl.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SplitLineControl : MonoBehaviour {
+
+	Image line;
+	Camera mainCam;
+	// Use this for initialization
+	void Start () {
+		line = this.gameObject.GetComponent<Image>();
+		mainCam = GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		line.enabled = !mainCam.enabled;
+	}
+}
