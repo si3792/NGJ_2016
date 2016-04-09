3b590b8cbb4abc447f1a91bcd7dac6315b96f9fc
Simo Iliev <si3792@h37.eduroam-2a.wlan.aalborguniversitet.dk>
2016-04-09 11:34:16 +0200

    Fixed null exceptions - enemy

--- Assets/Resources/Scripts/EnemyDamageScript.cs
+++ Assets/Resources/Scripts/EnemyDamageScript.cs
@@ -10,20 +10,19 @@ public class EnemyDamageScript : MonoBehaviour {
 	void Start () {
 	
 	}
-	
-	// Update is called once per frame
-	void LateFixedUpdate () {
 
-		if (health <= 0)
-			Instantiate (biomassDrop, transform.position, Quaternion.Euler (Vector3.zero));
-			Destroy (gameObject.transform.parent.gameObject);
-	}
 
 	void OnTriggerEnter2D(Collider2D other) {
 		if(other.gameObject.tag  == "Bullet") {
 			health -= other.gameObject.GetComponent<BulletMovement> ().damage;
 			Destroy (other.gameObject);
 		}
+
+		if (health <= 0) {
+			Instantiate (biomassDrop, transform.position, Quaternion.Euler (Vector3.zero));
+			Destroy (gameObject.transform.parent.gameObject);
+		}
+
 	}
 
 
