749b90741f79da94095eed674b487bdcbf91baac
Simo Iliev <si3792@h37.eduroam-2a.wlan.aalborguniversitet.dk>
2016-04-09 07:06:09 +0200

    Simple shooting for Player 2 added

---
+++ Assets/Resources/Scripts/RandomizeSpriteOnCommand.cs
@@ -0,0 +1,15 @@
+﻿﻿using UnityEngine;
+using System.Collections;
+
+public class RandomizeSpriteOnCommand : MonoBehaviour {
+
+	public Sprite[] mSprites;
+	public int mustEqSize;
+
+	public void randomize()
+	{
+		int a = Random.Range (0, mustEqSize);
+		this.gameObject.GetComponent<SpriteRenderer> ().sprite = mSprites [a];
+	}
+
+}
---
+++ Assets/Resources/Scripts/SimpleShoot.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleShoot : MonoBehaviour
+{
+
+	public float FireRate = 0.3f;
+	public float deviation;
+	public GameObject bullet;
+	public Transform muzzleTip;
+	float nextFire = 0;
+
+	// tmp reference to instantiated bullet
+	GameObject reff;
+
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+
+
+		if (Input.GetKey (KeyCode.Space) && Time.timeSinceLevelLoad > nextFire) {
+
+			nextFire = Time.timeSinceLevelLoad + FireRate;
+
+			if (gameObject.GetComponent<PlayerMovement> ().GetFacingRight ()) {
+				reff = (GameObject)Instantiate (bullet, muzzleTip.position, Quaternion.Euler (new Vector3(0, 0, -90 + Random.Range(-deviation, deviation))));
+			} else {
+				reff = (GameObject)Instantiate (bullet, muzzleTip.position, Quaternion.Euler (new Vector3(0, 0, 90 + Random.Range(-deviation, deviation))));
+			}
+
+			reff.GetComponent<RandomizeSpriteOnCommand> ().randomize ();
+		}
+	
+	}
+}
