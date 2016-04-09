933534bdc678937bb1212ba0c3e096dbbab0d746
Simo Iliev <si3792@h37.eduroam-2a.wlan.aalborguniversitet.dk>
2016-04-09 14:22:37 +0200

    player 2 can now die

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float health = 100;
+
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+
+		if(health <= 0) {
+			Destroy (transform.parent.gameObject);
+		}
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if(other.gameObject.tag == "BeetleHurt") {
+			//transform.parent.GetComponent<PlayerMovement> ().knockback (60, new Vector2 (transform.position.x - other.gameObject.transform.position.x, 0f));
+		}
+	}
+
+	void OnTriggerStay2D(Collider2D other) {
+		if(other.gameObject.tag == "BeetleHurt") {
+			//transform.parent.GetComponent<PlayerMovement> ().knockback (60, new Vector2 (transform.position.x - other.gameObject.transform.position.x, 0f));
+			health -= 10 * Time.fixedDeltaTime;
+		}
+	}
+
+}
