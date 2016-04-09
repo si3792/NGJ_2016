8103ac39acc3156c16f562149006acf44879cac3
Simo Iliev <si3792@h37.eduroam-2a.wlan.aalborguniversitet.dk>
2016-04-09 07:15:15 +0200

    Improved bullets

---
+++ Assets/Resources/Scripts/TimedDeath.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimedDeath : MonoBehaviour {
+
+	public float TimeToLive;
+	// Use this for initialization
+	void Start () {
+		Destroy (this.gameObject, TimeToLive);
+	}
+
+}
