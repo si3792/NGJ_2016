713a4e1a5aaa0fa158262e9e8589e24809a318c8
Rosen Zhelev <roro.omg@gmail.com>
2016-04-10 08:00:17 +0200

    Ship Nuggets

---
+++ Assets/Resources/Scripts/NuggetAtractor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NuggetAtractor : MonoBehaviour {
+
+	GameObject ship;
+	// Use this for initialization
+	float threshold= 4;
+
+	float getSmoothing(float dist) {
+		return threshold / dist;
+	}
+
+	void Start () {
+		ship = GameObject.FindGameObjectWithTag ("the-ship");
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		float dist = Vector3.Distance (transform.position, ship.transform.position);
+		if (dist < 0.1f) {
+			GameObject.Destroy (gameObject);
+			return;
+		}
+		transform.position = Vector3.Lerp (transform.position, ship.transform.position, getSmoothing(dist) * Time.deltaTime);
+
+	}
+}
---
+++ Assets/Resources/Scripts/NuggetExtract.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NuggetExtract : MonoBehaviour {
+
+	public GameObject extractedNugget;
+	public float extractTime = 0.25f;
+	float timeToExtract;
+	Collider2D zone;
+	GameObject pl1,pl2;
+	NuggetHandler pl1Nugs,pl2Nugs,nugs;
+	// Use this for initialization
+	void Start () {
+		timeToExtract = extractTime;
+		pl1 = GameObject.FindGameObjectWithTag ("Pl1");
+		pl2 = GameObject.FindGameObjectWithTag ("Pl2");
+		pl1Nugs = pl1.GetComponentInChildren<NuggetHandler> ();
+		pl2Nugs = pl2.GetComponentInChildren<NuggetHandler> ();
+		nugs = GetComponent<NuggetHandler> ();
+		zone = GetComponent<BoxCollider2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeToExtract -= Time.deltaTime;
+		if (timeToExtract <= 0.0f) {
+			timeToExtract = extractTime;
+			if (pl1 != null && zone.IsTouching (pl1.GetComponent<CircleCollider2D> ()) && pl1Nugs.nuggets > 0) {
+				pl1Nugs.nuggets--;
+				nugs.nuggets++;
+				GameObject.Instantiate (extractedNugget, pl1.transform.position, pl1.transform.rotation);
+			}
+			if (pl2 != null && zone.IsTouching (pl2.GetComponent<CircleCollider2D> ()) && pl2Nugs.nuggets > 0) {
+				pl2Nugs.nuggets--;
+				nugs.nuggets++;
+				GameObject.Instantiate (extractedNugget, pl2.transform.position, pl2.transform.rotation);
+			}
+		}
+	}
+}
