9e51c8d37b03b79bfe9efd9994c24906644d937c
Simo Iliev <si3792@h37.eduroam-2a.wlan.aalborguniversitet.dk>
2016-04-10 06:00:52 +0200

    psai init script

---
+++ Assets/EnableOnAwake.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnableOnAwake : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+		this.enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/psaiInit.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class psaiInit : MonoBehaviour {
+
+	public GameObject go;
+
+	void Awake () {
+		Instantiate (go, new Vector3 (10000f, 10000f, 10000f), transform.rotation);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
