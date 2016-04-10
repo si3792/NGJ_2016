8f214d3ef101d3b885c11a415402206ae1188402
Simo Iliev <si3792@h37.eduroam-2a.wlan.aalborguniversitet.dk>
2016-04-10 04:05:13 +0200

    Bullets knockback!

---
+++ Assets/BulletKnockback.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletKnockback : MonoBehaviour {
+
+	public float distance;
+	public float strength;
+	Vector2 direction;
+
+	void OnDestroy() {
+
+		foreach(GameObject go in GameObject.FindGameObjectsWithTag("BeetleHurt") ) {
+
+			if(Vector3.Distance(go.transform.position, transform.position) < distance) {
+
+				direction = go.transform.position - transform.position;
+				go.GetComponent<Rigidbody2D>(). AddForce(strength * direction * (1/Time.timeScale) * (1/distance), ForceMode2D.Impulse);
+
+			}
+
+		}
+
+	}
+
+}
