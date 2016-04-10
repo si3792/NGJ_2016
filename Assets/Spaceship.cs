0abdb600381592fc9e62bdbae083d03400b7ade2
Simo Iliev <si3792@h37.eduroam-2a.wlan.aalborguniversitet.dk>
2016-04-10 10:16:04 +0200

    ship works now

--- Assets/Spaceship.cs
+++ Assets/Spaceship.cs
@@ -4,6 +4,12 @@ using System.Collections;
 public class Spaceship : MonoBehaviour {
 
 	Animator anim;
+	int playersIn = 0;
+	bool readyToFly = false;
+
+	public void ready() {
+		readyToFly = true;
+	}
 
 	void Start() {
 		anim = GetComponent<Animator> ();
@@ -15,6 +21,25 @@ public class Spaceship : MonoBehaviour {
 
 	public void liftoff() {
 		anim.SetBool ("PlayerIn", true);
+	}
+
+
+	public void OnTriggerEnter2D(Collider2D other) {
+
+
+		if( readyToFly ) {
+			if(other.gameObject.tag == "Pl1" || other.gameObject.tag == "Pl2") {
+
+				Destroy (other.gameObject);
+				playersIn++;
+
+				if (playersIn == 2)
+					liftoff ();
+
+			}
+
+		}
+
 	}
 
 }
