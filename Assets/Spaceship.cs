d125008613051fb5dafe919bb59ef990aa4fb689
Simo Iliev <si3792@h37.eduroam-2a.wlan.aalborguniversitet.dk>
2016-04-10 10:00:42 +0200

    Spaceship state machine done

---
+++ Assets/Spaceship.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spaceship : MonoBehaviour {
+
+	Animator anim;
+
+	void Start() {
+		anim = GetComponent<Animator> ();
+	}
+
+	public void shipFueled() {
+		anim.SetBool ("ShipFueled", true);
+	}
+
+	public void liftoff() {
+		anim.SetBool ("PlayerIn", true);
+	}
+
+}
