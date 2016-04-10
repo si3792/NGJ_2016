099c3031af74afeea4342b828a8731fadf354615
Kiril Kafadarov <k.kafadarov@jacobs-university.de>
2016-04-10 12:50:37 +0200

    adsasgadf

--- Assets/Resources/Scripts/Camera/PerspectiveCameraSupervisor.cs
+++ Assets/Resources/Scripts/Camera/PerspectiveCameraSupervisor.cs
@@ -75,9 +75,9 @@ public class PerspectiveCameraSupervisor : MonoBehaviour {
 				break;
 		}
 		var oldPos = camObj.transform.position;
-		Debug.Log(z);
 		center.z = -(height * 0.5f / Mathf.Tan(camObj.fieldOfView * 0.5f * Mathf.Deg2Rad));
 		focalPoint.ForceFocus(center);
+		Debug.Log(center.z);
 		addDelayedExecution(time, new DelayedExecution( 
 			() => { 
 				camObj.transform.position = new Vector3(camObj.transform.position.x, camObj.transform.position.y, oldPos.z);
