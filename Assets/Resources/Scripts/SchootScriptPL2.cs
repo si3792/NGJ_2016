ccf3edb961897c7b50f1caecacb95f1e86ef842d
si3792 <simo.t.iliev@gmail.com>
2016-04-16 03:45:21 +0200

    Pl2 gun knockback added

--- Assets/Resources/Scripts/SchootScriptPL2.cs
+++ Assets/Resources/Scripts/SchootScriptPL2.cs
@@ -9,9 +9,11 @@ public class SchootScriptPL2 : MonoBehaviour {
 	GameObject gun;
 	public float deviation = 1;
 	public AudioClip shootSound;
+	GameObject pl;
 
 	void Start () {
 		gun = GameObject.FindGameObjectWithTag("pl2-gun");
+		pl = GameObject.FindGameObjectWithTag("Pl2");
 	}
 	
 	// Update is called once per frame
@@ -25,11 +27,11 @@ public class SchootScriptPL2 : MonoBehaviour {
 
 		if (transform.parent.gameObject.GetComponent<PlayerMovement> ().GetFacingRight ()) {
 			Instantiate (bullet, muzzleTip.position, Quaternion.Euler (new Vector3(0, 0, 180 + Random.Range(-deviation, deviation))));
-
+			pl.GetComponent<Rigidbody2D> ().AddForce (new Vector2 (-8f, 0), ForceMode2D.Impulse);
 			reff = (GameObject) Instantiate (muzzleFlash, gun.transform.position, Quaternion.Euler (new Vector3(0, 0, 0)));
 		} else {
 			Instantiate (bullet, muzzleTip.position, Quaternion.Euler (new Vector3(0, 0, 0 + Random.Range(-deviation, deviation))));
-
+			pl.GetComponent<Rigidbody2D> ().AddForce (new Vector2 (8f, 0), ForceMode2D.Impulse);
 			reff = (GameObject) Instantiate (muzzleFlash, gun.transform.position, Quaternion.Euler (new Vector3(0, 180, 0)));
 		}
 
