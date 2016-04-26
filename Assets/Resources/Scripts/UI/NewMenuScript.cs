56f7f3c2e86d9d823f09ad8172aa1d399b14db04
si3792 <simo.t.iliev@gmail.com>
2016-04-26 22:30:13 +0200

    Added simple scene transitioning effect

---
+++ Assets/Resources/Scripts/UI/NewMenuScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class NewMenuScript : MonoBehaviour {
+
+
+	public void ChangeScene (string sceneName){
+		StartCoroutine(DelayedSceneChange(sceneName, 0.7f));
+		GameObject.FindGameObjectWithTag ("Screenfader").GetComponent<ScreenfaderScript> ().DropCurtains ();
+	}
+
+
+	// Allow for fade to black transitions
+	IEnumerator DelayedSceneChange(string sceneName, float delayTime)
+	{
+		yield return new WaitForSeconds(delayTime);
+		SceneManager.LoadScene (sceneName);
+	}
+
+}
---
+++ Assets/Resources/Scripts/UI/ScreenfaderScript.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScreenfaderScript : MonoBehaviour {
+
+	public float fadeSpeedIn = 5f; 
+	public float fadeSpeedOut = 5f;
+	public bool sceneStarting = true, sceneEnding = false;
+	Image img;
+
+	void Start () {
+		img = GetComponent<Image> ();
+	}
+
+	void Update() {
+		if (sceneStarting)
+			Invoke ("StartingScene", 0.1f);
+
+		if (sceneEnding)
+			Invoke ("FadeToBlack", 0f);
+	}
+
+	void FadeToBlack() {
+		Color tmp = Color.Lerp (img.color, Color.black, fadeSpeedOut * Time.deltaTime);
+		img.color = tmp;
+	}
+
+	void FadeToClear() {
+		Color tmp = Color.Lerp (img.color, Color.clear, fadeSpeedIn * Time.deltaTime);
+		img.color = tmp;
+	}
+
+
+	void StartingScene () {
+		FadeToClear ();
+
+		if(img.color.a < 0.05f) {
+			img.color = Color.clear;
+			sceneStarting = false;
+		}
+
+	}
+
+
+	public void DropCurtains() {
+		sceneEnding = true;
+	}
+
+}
