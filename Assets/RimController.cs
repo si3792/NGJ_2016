4f351aad3e66f861b875343f8fa70479042765b9
si3792 <simo.t.iliev@gmail.com>
2016-05-01 20:23:24 +0200

    Working rim sparks

---
+++ Assets/RimController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RimController : MonoBehaviour {
+
+	public float chanceToSurge;
+	void Start () {
+		
+
+	}
+	
+
+	void Update () {
+
+		foreach(GameObject rim in GameObject.FindGameObjectsWithTag("Rim")) {
+			if (rim.GetComponent<RimScript> ().surging)
+				continue;
+
+			if(Random.value > (1-chanceToSurge)){
+				rim.GetComponent<RimScript> ().surging = true;
+				rim.GetComponent<Animator> ().SetTrigger ("surge");
+			}
+
+		}
+
+	}
+}
---
+++ Assets/RimScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RimScript : MonoBehaviour {
+
+	public bool surging = false;
+	void Start () {
+	
+	}
+
+	public void stopSurge() {
+		surging = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
