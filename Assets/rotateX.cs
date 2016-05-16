596f8980073ae2b18d7cd050ccf2d1a73b969691
si3792 <simo.t.iliev@gmail.com>
2016-05-16 23:33:27 +0200

    improved hall of fame coins

---
+++ Assets/rotateX.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotateX : MonoBehaviour {
+
+	public float speed;
+	public float rotX = 0;
+	public float rotY = 0;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.Rotate (new Vector3 (speed * Time.deltaTime * rotX, speed * Time.deltaTime * rotY, 0));
+	}
+}
