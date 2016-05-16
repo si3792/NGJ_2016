3e18a5cf0220379af1b3d101dae9e528c8265598
si3792 <simo.t.iliev@gmail.com>
2016-05-16 20:56:37 +0200

    Hp bar now shows numbers as well

--- Assets/Resources/Scripts/UI/HPBarScript.cs
+++ Assets/Resources/Scripts/UI/HPBarScript.cs
@@ -4,17 +4,26 @@ using UnityEngine.UI;
 public class HPBarScript : MonoBehaviour {
 
 	public string PlayerTag;
+	GameObject hpShow;
 	PlayerController hpObj;
 	void Start() {
 		hpObj = GameObject.FindGameObjectWithTag(PlayerTag).GetComponentInChildren<PlayerController>();
+
+		if (PlayerTag == "Pl1")
+			hpShow = GameObject.FindGameObjectWithTag ("blueHPShow");
+		else
+			hpShow = GameObject.FindGameObjectWithTag ("redHPShow");
 	}
 
 
 	void Update () {
 		if (hpObj != null) {
 			this.gameObject.GetComponent<Image>().fillAmount = hpObj.health / hpObj.MaxHP;
+
+			hpShow.GetComponent<Text>().text = Mathf.RoundToInt(hpObj.health).ToString()  + "/" + hpObj.MaxHP.ToString();
+
 		} else {
-			this.gameObject.GetComponent<Image>().fillAmount = 0.005f;
+			this.gameObject.GetComponent<Image> ().fillAmount = 0; //.001f;
 		}
 	}
 }
