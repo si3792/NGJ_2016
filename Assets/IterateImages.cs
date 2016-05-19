3367d492c7cbd4d96accbff9aeb0ccf0027c26a2
si3792 <simo.t.iliev@gmail.com>
2016-05-19 18:03:14 +0200

    Work on char selection screen

---
+++ Assets/IterateImages.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class IterateImages : MonoBehaviour {
+
+	// Iterates images, with delay on end (could be random from range)
+	public Sprite[] spr;
+	public float changeTime;
+	public bool endDelay;
+	public float delayMin;
+	public float delayMax;
+
+	Image img;
+	bool delayedFlag = false;
+	int cur = 0;
+ 
+	// Use this for initialization
+	void Start () {
+		img = GetComponent<Image> ();
+		InvokeRepeating ("next", 0f, changeTime);
+	}
+
+	void next(){
+
+		if (delayedFlag)
+			return;
+
+		img.sprite = spr [cur];
+		cur++;
+
+		if(cur >= spr.Length) {
+			cur = 0;
+			delayedFlag = true;
+			Invoke ("stopDelay", Random.Range(delayMin, delayMax));
+		}
+	}
+
+	void stopDelay(){
+		delayedFlag = false;
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
