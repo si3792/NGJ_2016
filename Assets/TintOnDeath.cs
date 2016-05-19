03d78d930ae8ccc4fa6a92dc413631f302f0d1e7
si3792 <simo.t.iliev@gmail.com>
2016-05-19 20:24:19 +0200

    Dynamic score avatars

---
+++ Assets/TintOnDeath.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TintOnDeath : MonoBehaviour {
+
+	public bool isPlayerOne;
+
+	void Start () {
+		if(GlobalData.p1survived == false && isPlayerOne) {
+			GetComponent<SpriteRenderer> ().color = Color.black;
+		}
+		else 
+		if(GlobalData.p2survived == false && isPlayerOne == false) {
+			GetComponent<SpriteRenderer> ().color = Color.black;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
