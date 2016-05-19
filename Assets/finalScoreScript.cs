1943d29c0113422dcc94a79e324d798d0fc0d638
si3792 <simo.t.iliev@gmail.com>
2016-05-19 20:45:02 +0200

    Scoring now works, full game loop

---
+++ Assets/finalScoreScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class finalScoreScript : MonoBehaviour {
+
+	Text txt;
+	public int finalScore;
+
+	void Start () {
+		txt = GetComponent<Text> ();	
+		finalScore = GlobalData.P1kills + GlobalData.P2kills + GlobalData.P1nuggets + GlobalData.P2nuggets;
+		txt.text = finalScore.ToString ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
