9b939fef2751a1bb85f8e048cd6c63997dad3832
si3792 <simo.t.iliev@gmail.com>
2016-05-25 23:55:24 +0200

    Score screen has working title

---
+++ Assets/TitleController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TitleController : MonoBehaviour {
+
+	public GameObject liftoff, death, fame;
+
+	void Start () {
+		if(GlobalData.gameMode == 2) {
+			fame.GetComponent<Image>().enabled = true;
+			return;
+		}
+
+
+		if (GlobalData.p1survived || GlobalData.p2survived){
+			liftoff.GetComponent<Image> ().enabled = true;
+			return;
+		}
+
+		death.GetComponent<Image> ().enabled = true;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
