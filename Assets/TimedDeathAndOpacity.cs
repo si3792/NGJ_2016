643fcbcafb53a4ad71ea32065344a964ba2c205f
si3792 <simo.t.iliev@gmail.com>
2016-07-11 16:41:01 +0200

    Nuggets now fade out

---
+++ Assets/TimedDeathAndOpacity.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimedDeathAndOpacity : MonoBehaviour {
+
+	public float timeToDie = 30f;
+	private SpriteRenderer mySR;
+	private float tmp;
+
+	void Start () {
+		tmp = timeToDie;
+		mySR = GetComponent<SpriteRenderer> ();
+		Destroy (this.gameObject, timeToDie);
+		InvokeRepeating ("ReduceOpacity", 1f, 1f);
+	}
+	
+
+	void ReduceOpacity () {
+		mySR.color = new Color (1f, 1f, 1f, tmp/timeToDie + 0.2f );
+		tmp--;
+	}
+}
